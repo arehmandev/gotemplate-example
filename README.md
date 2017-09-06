941a7332035d97f81c15e6b5e1a4ccca3c212a67
Abdul Rehman <arehmandev@outlook.com>
2017-09-06 12:04:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Example for using Gotemplate
+
+Hi this is just a tiny piece of code for using gotemplate - hope its useful to somebody
+
+go build
+
+./gotemplate-example
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"os"
+)
+
+var (
+	templatepath = "example.css.template"
+	newfilepath  = "example.css"
+)
+
+func main() {
+	// create the template
+	createtemplate(templatepath)
+
+	// create the file from the parsed template
+	parse(templatepath, newfilepath)
+
+}
+
+func createtemplate(createtemplatepath string) {
+	// create the template
+	f, _ := os.Create(templatepath)
+	f.Write([]byte("The text color is {{.textColor}} and the link color is {{.linkColorHover}}"))
+	f.Close()
+}
+
+func parse(parsedtemplate, resultingfile string) {
+	t, err := template.ParseFiles(parsedtemplate)
+	if err != nil {
+		log.Println("parsing file error", err)
+		return
+	}
+
+	f, err := os.Create(resultingfile)
+	if err != nil {
+		log.Println("creating file error: ", err)
+		return
+	}
+
+	// A sample config
+	config := map[string]string{
+		"textColor":      "#abcdef",
+		"linkColorHover": "#ffaacc",
+	}
+
+	err = t.Execute(f, config)
+	if err != nil {
+		log.Print("executing template error: ", err)
+		return
+	}
+
+	fnew, _ := os.Open(resultingfile)
+	io.Copy(os.Stdout, fnew)
+	f.Close()
+}
