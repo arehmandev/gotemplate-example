53e0ee27472ddcf2464d0640bcd2cf22f841f9ea
Abdul Rehman <arehmandev@outlook.com>
2017-09-07 15:22:25 +0100

    Added support for reading from YAML

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"text/template"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 var (
@@ -12,6 +15,8 @@ var (
 	templatecontents = "The text color is {{.textColor}} and the link color is {{.linkColorHover}}"
 	newfilepath      = "example.css"
 )
+
+type values map[string]interface{}
 
 func main() {
 	// create the template
@@ -43,9 +48,10 @@ func parse(parsedtemplate, resultingfile string) {
 	}
 
 	// A sample config
-	config := map[string]string{
-		"textColor":      "#abcdef",
-		"linkColorHover": "#ffaacc",
+	config, err := readValuesFile("config.yml")
+	if err != nil {
+		log.Print("executing template error: ", err)
+		return
 	}
 
 	err = t.Execute(f, config)
@@ -57,8 +63,26 @@ func parse(parsedtemplate, resultingfile string) {
 	fmt.Println("Template and Resultingfile produced:")
 	fmt.Printf("%v and %v were created \n", parsedtemplate, resultingfile)
 
+	// fmt.Println(config["testkey"])
+
 	// Uncomment to have the stdout (the contents of the newfile) on your console
 	// fnew, _ := os.Open(resultingfile)
 	// io.Copy(os.Stdout, fnew)
 	// f.Close()
+}
+
+func readValuesFile(filename string) (values, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return map[string]interface{}{}, err
+	}
+	return readValues(data)
+}
+
+func readValues(data []byte) (vals values, err error) {
+	err = yaml.Unmarshal(data, &vals)
+	if len(vals) == 0 {
+		vals = values{}
+	}
+	return
 }
